77d4287c7e7aa4fa2a219e02e2e40ad53ff3b004
vinodhinivin <vinodhinisp>
2022-03-29 20:25:04 +0530

    done

---
+++ src/org/bankaccount/Sample.java
@@ -0,0 +1,37 @@
+package org.bankaccount;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		String fullName = "S.P.Vinodhini";
+		System.out.println("Fullname:" +fullName);
+		
+		int customerId = 87654312;
+		System.out.println("Customer Id:" +customerId);
+		
+		long accountNumber = 206199098657l;
+		System.out.println("Acoount Number:" +accountNumber);
+		
+		short branchCode = 1234;
+		System.out.println("Branch Code:" +branchCode);
+		
+		String panNo = "AYBUN3377K";
+		System.out.println("PAN NO:" +panNo);
+		
+		String address = "ABC Emerald Apartment, Flat no -B3, Porur";
+		System.out.println("Address:" +address);
+		
+		int pinCode = 600122;
+		System.out.println("Pnicode:" +pinCode);
+		
+		long mobNo = 9004567386l;
+		System.out.println("Mobile nO:" +mobNo);
+		
+		String mailId = "vin.123@gmail.com";
+		System.out.println("Mail Id:" +mailId);
+		
+		boolean toApplyForNetBanking = true;
+		System.out.println("I wish to apply for Netbanking:" +toApplyForNetBanking);
+	} 
+
+}
